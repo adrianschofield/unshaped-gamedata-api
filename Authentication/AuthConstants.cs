e62d03ead1bdabb1eb2da25d9f3f0273c5153508
Adrian Schofield <adrianschofield@hotmail.co.uk>
2024-02-06 18:56:20 +0000

    Adding authentication using AWS Secrets

---
+++ Authentication/AuthConstants.cs
@@ -0,0 +1,7 @@
+namespace unshaped_gamedata_api.Authentication;
+
+public static class AuthConstants
+{
+    public const string ApiKeySectionName = "Authentication:ApiKey";
+    public const string ApiKeyHeaderName = "X-Api-Key";
+}
--- Program.cs
+++ Program.cs
@@ -1,14 +1,37 @@
+using Amazon;
+using Microsoft.Build.Framework;
+using unshaped_gamedata_api.Authentication;
 using unshaped_gamedata_api.Data;
+
+
 
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("Games") ?? "Data Source=Data/Games.db";
 // Add services to the container.
-
 builder.Services.AddControllers();
 builder.Services.AddSqlite<DatabaseContext>(connectionString);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add support for AWS Secrets Manager
+var env = builder.Environment.EnvironmentName;
+var appName = builder.Environment.ApplicationName;
+
+// replace dashes with dots (AWS Secrets manager does not allow dash)
+appName = appName.Replace("-", ".");
+
+builder.Configuration.AddSecretsManager(region: RegionEndpoint.EUWest2,
+    configurator: options =>
+    {
+        // Filter out any unrequired secrets
+        options.SecretFilter = entry => entry.Name.StartsWith($"{env}_{appName}_");
+        // provide secrets in Configuration format e.g. Database:ConnectionString
+        options.KeyGenerator = (entry, s) =>    s
+            .Replace($"{env}_{appName}_", string.Empty)
+            .Replace("__", ":");
+    }
+);
 
 var app = builder.Build();
 
@@ -20,6 +43,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseMiddleware<ApiKeyAuthMiddleware>();
 
 app.UseAuthorization();
 
