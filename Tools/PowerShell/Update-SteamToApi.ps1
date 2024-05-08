3b95db5d858611c37a795162a94e1cc4eebfdc68
Adrian Schofield <adrianschofield@hotmail.co.uk>
2024-05-08 11:02:23 +0100

    Script to update the REST API from Steam

--- Tools/PowerShell/Update-SteamToApi.ps1
+++ Tools/PowerShell/Update-SteamToApi.ps1
@@ -2,7 +2,7 @@
 
 # Globals
 $SteamBaseUri = "https://api.steampowered.com/"
-$ApiBaseUri = "https://localhost:7011/api/"
+$ApiBaseUri = "https://app-gamedata-westeurope-dev-001.azurewebsites.net/api/"
 
 # Functions
 
@@ -28,6 +28,22 @@ function GetDataFromSteam($key, $steamId) {
 }
 
 # API
+
+function GetGameData($apiKey) {
+
+    $uri = $ApiBaseUri + "gamedata"
+    $headers = @{"Content-Type" = "application/json"; "x-api-key" = $apiKey}
+    $results = $null
+
+    try {
+        $results = Invoke-RestMethod -Uri $uri -Method Get -Headers $headers
+    } catch {
+        Write-Host "Failed with an error " + $_.ErrorDetails.Message
+    }
+    return $results
+
+}
+
 function CreateJsonObjectForApi($name, $platform, $timePlayed, $like) {
     
     $jsonBase = @{}
@@ -52,15 +68,37 @@ function CreateJsonObjectForApi($name, $platform, $timePlayed, $like) {
 
 }
 
-function PutGameDataApi($entry, $apiKey) {
+function UpdateJsonObjectForApi($entry) {
+    # Using this function means that the time played data needs to be updated
+    $entry.minutes = $entry.timePlayed % 60
+    $entry.hours = ($entry.timePlayed - $entry.minutes) / 60
+}
+
+# Create a new entry in the API
+function PostGameDataApi($entry, $apiKey) {
     $uri = $ApiBaseUri + "gamedata"
-    # And we need Headers making sure to add Notion-Version
+    # And we need Headers making sure to add API Key
     $headers = @{"Content-Type" = "application/json"; "x-api-key" = $apiKey}
 
     try {
         Invoke-RestMethod -Uri $uri -Method Post -Body ($entry | ConvertTo-Json -Depth 2) -Header $headers
     } catch {
-        Write-Host "Failed with an error " + $_.ErrorDetails.Message
+        Write-Host "Failed create new entry with error " + $_.ErrorDetails.Message
+    }
+}
+
+# Update an existing entry in the API
+function PutGameDataApi($entry, $apiKey) {
+    # Note uri requires the id of the object to be updated
+    $uri = $ApiBaseUri + "gamedata/" + $entry.id
+
+    # And we need Headers making sure to add API Key
+    $headers = @{"Content-Type" = "application/json"; "x-api-key" = $apiKey}
+
+    try {
+        Invoke-RestMethod -Uri $uri -Method Put -Body ($entry | ConvertTo-Json -Depth 2) -Header $headers
+    } catch {
+        Write-Host "Failed update existing entry with error " + $_.ErrorDetails.Message
     }
 }
 
@@ -75,18 +113,43 @@ function LoadConfig() {
 # First thing I need to do is load the configuration for the API Token
 $config = LoadConfig
 
-# I also need the api key get it from AWS in this case
-$awsSecret = Get-SECSecretValue -SecretId Development_unshaped.gamedata.api_Authentication__ApiKey
-$apiKey = $awsSecret.SecretString
+# We need to retrieve the api key from Azure Key Vault rather than the configuration file
+$apiKey = Get-AzKeyVaultSecret -VaultName $config.azKeyVault -Name $config.azKeyName -AsPlainText
 
 # Let's get the info from Steam
 $steamResults = GetDataFromSteam -key $config.steamKey -steamId $config.steamId
 
-# Now I need to loop through that lot and create an API object for each one
-# and then PUT it into the database
+# And read data from the API
+$apiResults = GetGameData -apiKey $apiKey
+
+# Now I need to loop through the steam data
+# Find each game in the API Data
+# If the game doesn't exist add it to the API
+# If the game is different then update the API
 
 foreach ($steamResult in $steamResults.response.games) {
-    $apiObject = CreateJsonObjectForApi -name $steamResult.name -platform "PC" -timePlayed $steamResult.playtime_forever -like $true
-    $result = PutGameDataApi -entry $apiObject -apiKey $apiKey
+    $matched = $false
+    foreach($apiResult in $apiResults){
+        if ($steamResult.name -eq $apiResult.name) {
+            # Found a match
+            $matched = $true
+            # Check for changes, the only thing that would really change is Time Played
+            if ($steamResult.playtime_forever -gt $apiResult.timePlayed) {
+                # DBG
+                Write-Host "Would update"  $apiResult.name "because times are different:"  $apiResult.timePlayed " :" $steamResult.playtime_forever
+                $apiResult.timePlayed = $steamResult.playtime_forever
+                UpdateJsonObjectForApi -entry $apiResult
+                $result = PutGameDataApi -entry $apiResult -apiKey $apiKey
+            }
+        }
+    }
+
+    if ($matched -eq $false) {
+        # DBG
+        Write-Host "Didn't find an entry in the API for" $steamResult.name
+        # Update the API as this Steam game was not found
+        $apiObject = CreateJsonObjectForApi -name $steamResult.name -platform "PC" -timePlayed $steamResult.playtime_forever -like $true
+        $result = PostGameDataApi -entry $apiObject -apiKey $apiKey
+    }
 }
 
